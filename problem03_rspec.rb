0b405990cbd11bd8ea0a463594c5f49a92615edf
kvattikuti <kalyana.vattikuti@gmail.com>
2014-07-27 05:09:19 -0400

    test cases

--- problem03_rspec.rb
+++ problem03_rspec.rb
@@ -10,4 +10,8 @@ describe 'largest prime factor' do
 	it 'finds 5 as largest prime factor for input of 100' do
 		expect(Problem03Module.main([100])).to eq(5)
 	end
+	it 'mocks and stubs' do
+		double('ClassX').class
+		print instance_double("ClassX", :pages => 250).class.pretty_print_instance_variables
+	end
 end
